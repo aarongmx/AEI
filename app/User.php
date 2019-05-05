7b0233c37f44535f03db642b72f3061f5de8fbc7
Aaron Gomez Mendez <aaron.gomez.mendez@gmail.com>
2019-05-05 18:22:45 -0500

    Configuración de backend con jwt

--- app/User.php
+++ app/User.php
@@ -2,20 +2,31 @@
 
 namespace App;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
+    protected $primaryKey = 'usuario_id';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'confirmation_code',
+        'nombres',
+        'apellido_paterno',
+        'apellido_materno',
+        'usuario_asociado',
+        'usuario_administrador',
+        'cuenta_activa',
+        'notificaciones',
+        'email',
+        'password',
     ];
 
     /**
@@ -24,6 +35,33 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'confirmation_code',
+        'usuario_asociado',
+        'usuario_administrador',
+        'cuenta_activa',
+        'notificaciones',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
 }
